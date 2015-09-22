42107139d64763aeab93369d3f712e0c5f34d8f2
T-107 <T-107@PC244>
2015-09-21 19:40:19 -0500

    =170915=

--- lenguajes-macielaguilar/src/lenguajes/macielaguilar/Imc.java
+++ lenguajes-macielaguilar/src/lenguajes/macielaguilar/Imc.java
@@ -12,10 +12,30 @@ package lenguajes.macielaguilar;
 public class Imc {
     float peso;
     float altura;
+    
     public Imc(){
     
-    peso=98f;
-            altura=1.79f;
+        peso=98f;
+        altura=1.79f;
+    
+    }
+    public Imc(float f){
+    //este codigo no tiene nada que ver con los constructores
+        byte b=2;
+        short s=2;
+        int i=2;
+        long l=2;
+        
+        //reasignamos
+        b=(byte)i;
+        l=i;
+        s=(short)l;
+        s=b;
+        l=s;
+        s=(short)l;
+        s=(short)i;
+        l=b;
+        
     }
     public float calcular(){
         float resultadoImc=peso/(altura*altura);
