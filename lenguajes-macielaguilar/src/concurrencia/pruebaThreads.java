ed338eaa9c803478d2c0a0e48561c62252a8fd0d
T-107 <T-107@PC260>
2015-11-09 19:42:41 -0600

    clase thread Runnable

--- lenguajes-macielaguilar/src/concurrencia/pruebaThreads.java
+++ lenguajes-macielaguilar/src/concurrencia/pruebaThreads.java
@@ -6,15 +6,24 @@
 package concurrencia;
 
 
-public class pruebaThreads {
+public class pruebaThreads implements Runnable {
     public static void main(String[] args) {
         
         //primer pso creado
         primerThread t1=new primerThread();
         //segundo paso iniciado
+       // t1.start();
+        
+        pruebaThreads r=new pruebaThreads();
+        Runnable r2=new pruebaThreads();//referencia de un tipo y la instancia de otro tipo
+        Thread t2=new Thread(r2);//
+        
         t1.start();
+        t2.start();
     }
-  
+  public void run(){
+      System.out.println("Soy el segundo Thread");
+  }
     } 
     
 
